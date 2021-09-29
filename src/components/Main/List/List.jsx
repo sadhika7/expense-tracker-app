9374ff5e127ef38b4f744dd38ac8a7f78c950512
sadhika7 <swatiadhikari1997@gmail.com>
2021-09-28 23:37:54 -0400

    Adding context to the application

--- src/components/Main/List/List.jsx
+++ src/components/Main/List/List.jsx
@@ -2,13 +2,19 @@ import React from 'react';
 import {List as MUIList, ListItem, ListItemAvatar, ListItemText, Avatar, ListItemSecondaryAction, IconButton, Slide} from '@material-ui/core';
 import {Delete, MoneyOff} from '@material-ui/icons';
 
+import {ExpenseTrackerContext} from '../../../context/context';
 import useStyles from './styles';
 
 const List = () => {
     const classes = useStyles();
+    // const globalState = useContext(ExpenseTrackerContext);
+
+    // console.log(globalState);
 
     const transactions = [
-        {id: 1, type: "Income", category:"Salary", amount:50, date: new Date()}
+        {id: 1, type: "Income", category:"Salary", amount:50, date: "Tues Sept 29"},
+        {id: 2, type: "Expense", category:"Pets", amount:50, date: "Tues Sept 28"},
+        {id: 3, type: "Income", category:"Business", amount:150, date: "Wed Sept 29"},
     ];
 
     return (
