c7bc2d996bc4f36f37c1c75be0571ad6efa2c44f
sadhika7 <swatiadhikari1997@gmail.com>
2021-10-04 15:11:18 -0400

    Added Push to talk button from Speechly

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {SpeechProvider} from '@speechly/react-client';
 
 import {Provider} from './context/context';
 import App from './App';
 import './index.css';
 
 ReactDOM.render(
+    <SpeechProvider appId="bbdd0708-a63d-4756-934a-40aeee6be3de" language="en-US">
     <Provider>
         <App />
-    </Provider>,
+    </Provider>
+    </SpeechProvider>,
 document.getElementById('root'));
