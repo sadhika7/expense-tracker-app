408c40aaeb45d940374296257beb0c17bcb5c3ba
sadhika7 <swatiadhikari1997@gmail.com>
2021-10-13 19:10:13 -0400

    Fixed Issues with Expense Doughnut

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ const App = () => {
               <Grid item xs={12} sm={4} className={classes.desktop}>
                 <Details title="Income"/>
               </Grid>
-              <Grid item xs={12} sm={4} className={classes.desktop}>
+              <Grid item xs={12} sm={4} className={classes.last}>
                 <Details title="Expense"/>
               </Grid>
           </Grid>
